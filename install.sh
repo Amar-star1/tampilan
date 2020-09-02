b5473a268fc122cec75c8ea18fed1963f33c6cfe
Dhika <65761933+Amar-star1@users.noreply.github.com>
2020-09-02 13:48:27 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,14 @@
+pkg install python
+clear
+pkg install python2
+clear
+pip2 install lolcat
+clear
+pkg install figlet
+clear
+figlet -f slant ' WELCOME' | lolcat
+echo " [!] Tools By : AMAR DHIKA" | lolcat
+echo " [!] FACEBOOK : Dhika Vhanhouthen" | lolcat
+echo " [!] Whatsapp : 083805408276" | lolcat
+echo " [!] THX " | lolcat
+
